9aa653bea04b33e10641fb5ecd64f477d4f526c6
Henry Nguyen <>
2023-10-23 21:39:58 +0700

    fail to run on production

--- recommendation/README.md
+++ recommendation/README.md
@@ -45,3 +45,52 @@ To run the tests, use the command:
 `pytest`
 
 Feel free to modify and improve the service as needed.
+
+## Run the Docker containers using
+
+`docker-compose up -d.`
+
+This setup will distribute the incoming traffic across the two instances of recommendation service, providing a basic load balancing mechanism. You can scale up the number of instances and fine-tune the load balancing configuration to suit specific requirements.
+
+set up load balancing using HAProxy:
+
+1. **Install HAProxy on your server if it's not already installed:**
+
+```
+sudo apt-get update
+sudo apt-get install haproxy
+
+```
+
+2. **Configure the HAProxy settings:**
+
+Edit the HAProxy configuration file, typically located at `/etc/haproxy/haproxy.cfg`.
+
+`sudo nano /etc/haproxy/haproxy.cfg`
+
+Add the following configuration to set up load balancing for two backend servers:
+
+```
+frontend myapp
+    bind *:80
+    mode http
+    default_backend myapp
+
+backend myapp
+    mode http
+    balance roundrobin
+    option httpchk HEAD / HTTP/1.1\r\nHost:localhost
+    server backend1 your_server_ip1:8000 check
+    server backend2 your_server_ip2:8000 check
+
+```
+
+3. **Save the configuration file and restart HAProxy:**
+
+`sudo service haproxy restart`
+
+Make sure to replace your_server_ip1 and your_server_ip2 with the actual IP addresses of your backend servers.
+
+With this configuration, HAProxy will balance the traffic between the two backend servers, providing high availability and ensuring that if one server goes down, the other server can handle the requests.
+
+Additionally, you may need to configure your firewall settings to allow traffic on the appropriate ports (e.g., port 80) for HAProxy and your backend servers.
--- recommendation/services.py
+++ recommendation/services.py
@@ -2,14 +2,48 @@ import os
 from dotenv import load_dotenv
 import requests
 import pandas as pd
-from fastapi import  HTTPException, Depends
+from fastapi import HTTPException, Depends
 from surprise import Dataset, Reader, SVD
 from surprise.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
+import functools
+from cachetools import cached, TTLCache
+import asyncio
+import aiohttp
+import logging
+from pydantic import BaseModel, Field
+from .schema.user import UserSchema
+from .schema.product import ProductSchema
+from .schema.transactions import TransactionSchema
+from bleach import clean
+
 # Load environment variables from the .env file
 load_dotenv()
 API_KEY = os.getenv("API_KEY")
-# Define headers with access token
+
+
+# Define the cache size and time-to-live (TTL) for the cache
+cache = TTLCache(
+    maxsize=100, ttl=300
+)  # Adjust the values according to your requirements
+
+
+# Validate user input
+def validate_user_data(user_data: dict):
+    validated_data = UserSchema(**user_data)
+    return validated_data
+
+
+# Validate product input
+def validate_product_data(product_data: dict):
+    validated_data = ProductSchema(**user_data)
+    return validated_data
+
+
+# Validate transaction input
+def validate_transaction_data(transaction_data: dict):
+    validated_data = TransactionSchema(**user_data)
+    return validated_data
 
 
 class RecommendationService:
@@ -26,25 +60,48 @@ class RecommendationService:
             "Books": 4,
             "Food": 5,
         }
-        self.headers ={"access_token": API_KEY, "Content-Type": "application/json"}
+        self.headers = {"access_token": API_KEY, "Content-Type": "application/json"}
 
     async def get_api_key(self, api_key):
         if api_key != self.API_KEY:
-            raise HTTPException(status_code=HTTP_403_FORBIDDEN, detail="Invalid API Key")
+            raise HTTPException(
+                status_code=HTTP_403_FORBIDDEN, detail="Invalid API Key"
+            )
         return api_key
 
-    async def get_recommendations(self, user_id: int, api_key: str = Depends(get_api_key)):
+    @functools.lru_cache(maxsize=128)
+    @cached(cache)  # This decorator caches the results of the function
+    async def get_recommendations(
+        self, user_id: int, api_key: str = Depends(get_api_key)
+    ):
         reader = Reader(rating_scale=(1, 5))
         try:
-            # Fetch user data
-            user_data = requests.get(self.USER_URL, headers=self.headers).json()
+            async with aiohttp.ClientSession() as session:
+                async with session.get(self.USER_URL, headers=self.headers) as response:
+                    # Fetch user data
+                    user_data = await response.json()
+                async with session.get(
+                    self.PRODUCT_URL, headers=self.headers
+                ) as response:
+                    # Fetch product data
+                    product_data = await response.json()
+                async with session.get(
+                    self.TRANSACTION_URL, headers=self.headers
+                ) as response:
+                    transaction_data = await response.json()
 
-            # Fetch product data
-            product_data = requests.get(self.PRODUCT_URL, headers=self.headers).json()
+            # Validate user data
+            validated_user_data = [validate_user_data(data) for data in user_data]
 
-            # Fetch transaction history
-            transaction_data = requests.get(self.TRANSACTION_URL, headers=self.headers).json()
+            # Validate product data
+            validated_product_data = [
+                validate_product_data(data) for data in product_data
+            ]
 
+            # Validate transactions data
+            validated_transaction_data = [
+                validate_transaction_data(data) for data in transaction_data
+            ]
             # Extract preferences from user data
             preferences = [user["preferences"] for user in user_data]
 
@@ -64,25 +121,41 @@ class RecommendationService:
             transaction_data_frame = pd.DataFrame(transaction_data)
 
             # Merge dataframes based on common columns
-            merged_data = pd.merge(user_data_frame, transaction_data_frame, on="user_id")
+            merged_data = pd.merge(
+                user_data_frame, transaction_data_frame, on="user_id"
+            )
             final_data = pd.merge(
-                merged_data, product_data_frame, left_on="product_id", right_on="product_id"
+                merged_data,
+                product_data_frame,
+                left_on="product_id",
+                right_on="product_id",
             )
 
             data = Dataset.load_from_df(
                 final_data[["user_id", "product_id", "numerical_preferences"]], reader
             )
+        except aiohttp.ClientError as client_error:
+            logging.error(f"Aiohttp client error: {client_error}")
+            raise HTTPException(status_code=500, detail="Internal server error")
+
         except Exception as e:
+            logging.error(f"An error occurred: {e}")
+            raise HTTPException(status_code=500, detail="Internal server error")
+
             print(f"Failed to load data from API. Loading data from CSV. Error: {e}")
             final_data = pd.read_csv("dataset.csv")
-            final_data["preferences"] = final_data["preferences"].map(self.preference_mapping)
+            # Validate the data
+            if final_data.isnull().values.any():
+                raise ValueError("Null values found in the dataset")
+            final_data["preferences"] = final_data["preferences"].map(
+                self.preference_mapping
+            )
 
             # Load data into Surprise (example code)
             reader = Reader(rating_scale=(1, 5))
             data = Dataset.load_from_df(
                 final_data[["user_id", "product_id", "preferences"]], reader
             )
-
 
         trainset, testset = train_test_split(data, test_size=0.2)
         algo = SVD()
